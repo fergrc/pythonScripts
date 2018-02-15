d31fa48edbd93166310f617ef039e72c79c98028
fergrc <32725847+fergrc@users.noreply.github.com>
2018-02-15 15:51:17 -0800

    Update createTreads.py

--- createTreads.py
+++ createTreads.py
@@ -1,12 +1,11 @@
 '''
-Automates the process of connecting nodes in order to create treads for tank rigs
+Automates the process of connecting nodes in order to create treads for tank rigs.
 by Fernanda Coelho
 ''' 
 import pymel.core as pm
 
-pm.currentUnit(linear='cm')
-# make empty list for joints that will be added later
-treadJnts = []
+pm.currentUnit(linear='cm') # set units to cm 
+treadJnts = [] # make empty list for joints that will be added later
 
 # UI callbacks:
 # function that sets the curve we're gonna use for the treads
@@ -22,6 +21,7 @@ def setJntsList(*args):
 	print treadJnts
 	jntsInput.setText('Set!')
 
+# if the user wishes to, they can delete the motionPaths with a UI button
 def deleteMotionPaths(*args):
 	try:
 		for j in treadJnts:
@@ -30,33 +30,33 @@ def deleteMotionPaths(*args):
 	except:
 		pass
 
-		
+# function that makes the treads
 def makeTreads(*args):
 	try:
-		if('world_object_up'): # if locator exists from a previous attempt, delete and create it again
+		if('world_object_up'): # if locator exists from a previous attempt, delete it
 			pm.delete('world_object_up')  
 			pm.spaceLocator(name='world_object_up')
 			pm.addAttr(ln="Tread_Cycle", at='double', dv=0, hidden=False, k=True)
 			pm.select(clear=True)
 			pm.parentConstraint('treadCurve', 'world_object_up') 
 	except:
-		# or esle just create it
-		pm.spaceLocator(name='world_object_up')
+		pm.spaceLocator(name='world_object_up') # or else just create it
 		pm.addAttr(ln="Tread_Cycle", at='double', dv=0, hidden=False, k=True)
 		pm.select(clear=True)
 		pm.parentConstraint('treadCurve', 'world_object_up')
 	
 	try:
-		for j in treadJnts:
+		for j in treadJnts: # if motionPaths exist from a previous attempt, delete them
 			if('motionPath' + str(treadJnts.index(j)+1)):
 				pm.delete('motionPath' + str(treadJnts.index(j)+1))
 	except:
 		pass
 		
-
+	# get the Max Value of the curve, so the treads loop properly
 	curveMaxValue = pm.getAttr('treadCurve.maxValue')
 	print curveMaxValue		 
-	# this loop will go through each joint in the list
+	
+	# this loop will go through each joint in the treadJnts list
 	for i in treadJnts:
 		# for the first joint, which has index '0'
 		if treadJnts.index(str(i)) == 0:
@@ -70,49 +70,50 @@ def makeTreads(*args):
 			pm.delete('addDoubleLinear1', 'addDoubleLinear2', 'addDoubleLinear3','motionPath1_uValue')
 			# connect motionPath allCoordinates node into the joint's translates 
 			pm.connectAttr('motionPath1.allCoordinates', str(i) + '.translate', force=True) 
-			pm.setAttr('world_object_up.Tread_Cycle', 0)
+			# set driven keys for the anim curve driving the joints around the curve
+			pm.setAttr('world_object_up.Tread_Cycle', 0) 
 			pm.setAttr('motionPath1.uValue', 0)
 			pm.setDrivenKeyframe('motionPath1.uValue', currentDriver='world_object_up.Tread_Cycle')
 			pm.setAttr('world_object_up.Tread_Cycle', curveMaxValue)
 			pm.setAttr('motionPath1.uValue', curveMaxValue)
 			pm.setDrivenKeyframe('motionPath1.uValue', currentDriver='world_object_up.Tread_Cycle')
 			pm.selectKey('motionPath1.uValue')
-			pm.keyTangent(itt='linear', ott='linear')
+			pm.keyTangent(itt='linear', ott='linear') # make them linear and cycle
 			pm.setInfinity(pri='cycle', poi='cycle')
 			pm.select(clear=True)
-			pm.rename('motionPath1_uValue', 'setDrivenKeys_anim_1')
+			pm.rename('motionPath1_uValue', 'setDrivenKeys_anim_1') # rename it to something with number at the end, easier to work with if it's duplicated
 			
 			# for the rest of the joints:
 		else: 
-			# attach joint to curve with a motionPath constraint
+			# constrain the joint to the curve using motion path animation
 			pm.select(clear=True)
 			pm.select(i)
 			pm.select('treadCurve', add=True)
 			pm.pathAnimation(fractionMode=False, follow=True, followAxis='y', upAxis='z', worldUpType='object', worldUpObject='world_object_up', inverseUp=True, inverseFront=False, bank=False)
 			pm.select(clear=True)
-			# delete useless doubleLiner nodes
+			# delete useless nodes we're not gonna need
 			pm.delete('addDoubleLinear1', 'addDoubleLinear2', 'addDoubleLinear3','motionPath'+str(treadJnts.index(i)+1)+'_uValue')
 			# connect motionPath allCoordinates node into joint's translates 
 			pm.connectAttr('motionPath'+str(treadJnts.index(i)+1)+'.allCoordinates', str(i) + '.translate', force=True)
-			# delete motionPath_uValue, created by default when the joint is constrained through a motionPath
 			pm.select(clear=True)
-			# duplicate the animation curve we made with the setDrivenKeys
+			# duplicate the previous set driven keys anim curve
 			pm.select('setDrivenKeys_anim_'+ str(treadJnts.index(i)))
 			pm.duplicate()
 			pm.select(clear=True)
-			# connect the new duplicated drivenKeys animation curve's output into the joint's motionPath's uValue
+			# connect the new duplicated setDrivenKeys anim's output into the joint's motionPath's uValue
 			pm.connectAttr('setDrivenKeys_anim_'+str(treadJnts.index(i)+1)+'.output', 'motionPath'+str(treadJnts.index(i)+1)+'.uValue', force=True)
 			# connect the animation curve to the Tread Cycle attribute
 			pm.connectAttr('world_object_up.Tread_Cycle', 'setDrivenKeys_anim_'+str(treadJnts.index(i)+1)+'.input', force=True)
 			# shift the animation curve to the side so the treads won't be on top of each other and move accordingly 
 			pm.select('setDrivenKeys_anim_'+str(treadJnts.index(i)+1))
 			pm.selectKey()
-			num = spaceInput.getValue1()
+			num = spaceInput.getValue1() # get the spacing the user wants
 			pm.keyframe(option='over', relative=True, floatChange=(num))
 			pm.select(clear=True)
 		
 #################################################################################################
 # UI CODE
+
 # if UI window already exists, kill it
 try:
 	pm.window('Create Treads', exists = True)
@@ -129,3 +130,5 @@ jntsInput = pm.textFieldButtonGrp(text="Select Tread jnts and Set", bl='Set' , p
 spaceInput = pm.floatFieldGrp(nf=1, l='Offset',p=column, precision=3)
 makeTreadsButton = pm.button(l='Make Treads', p=column, c=makeTreads)
 deleteMotionPaths = pm.button(l='Delete Motion Paths', p=column, c=deleteMotionPaths)
+
+#################################################################################################
